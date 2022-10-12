d4e8e2c88ef2b7801898734d1abaf28aff1d5579
DDongYeop <kitty-777@naver.com>
2022-10-12 20:28:09 +0900

    타임라인, 암전 보완

--- Assets/01.Script/Dongyeop/TimeLine/TimeLineEnd.cs
+++ Assets/01.Script/Dongyeop/TimeLine/TimeLineEnd.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.UI;
 
 public class TimeLineEnd : MonoBehaviour //타임라인 종료시 실행
 {
@@ -10,17 +11,27 @@ public class TimeLineEnd : MonoBehaviour //타임라인 종료시 실행
     private Focus _focus;
     private Flicker _flicker;
 
+    private Image _blackImage;
+
     private void Awake()
     {
         _focus = GetComponent<Focus>();
         _flicker = GetComponent<Flicker>();
+        _blackImage = GameObject.Find("BlackImage").GetComponent<Image>();
     }
 
     public void TimeLineend()
     {
         _focus.postProcess.profile = _noneProfile;
-        _focus.enabled = false;
+        
+        _flicker.eyeImage.enabled = false;
 
+        _focus.enabled = false;
         _flicker.enabled = false;
+    }
+
+    public void TimeLineFadeOutEnd()
+    {
+        _blackImage.enabled = false;
     }
 }
