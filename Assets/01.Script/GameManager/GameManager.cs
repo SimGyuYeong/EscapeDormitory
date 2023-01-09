d76ec9a2ead90a55dc5619d2898cbf65c4557fda
SonSeongBin <thstjdqls2625@gmail.com>
2023-01-09 12:38:31 +0900

    monster

--- Assets/01.Script/GameManager/GameManager.cs
+++ Assets/01.Script/GameManager/GameManager.cs
@@ -1,8 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoSingleTon<GameManager>
 {
+    public TMP_Text bettery;
+    private int betteryAmount = 100;
+    
+    public int BetteryAmount
+    {
+        get { return betteryAmount; }
+        set
+        {
+            betteryAmount = value;
+            bettery.text = BetteryAmount.ToString() + "%";
+        }
+    }
+    WaitForSeconds WaitForSeconds = new WaitForSeconds(5f);
 
+    private void Start()
+    {
+        StartCoroutine(BetteryDown());
+    }
+
+    public IEnumerator BetteryDown()
+    {
+        while (true)
+        {
+            
+            yield return WaitForSeconds;
+            BetteryAmount--;
+
+
+
+        }
+    }
 }
